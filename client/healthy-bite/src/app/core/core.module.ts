140be43a373916d776accd16be56116900a8c707
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 21:04:58 +0200

    Add footer component in core module

--- client/healthy-bite/src/app/core/core.module.ts
+++ client/healthy-bite/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
