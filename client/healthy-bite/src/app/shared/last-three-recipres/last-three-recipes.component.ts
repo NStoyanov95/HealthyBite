10287b53aa8f2195e54915a2362ffc71526b0e85
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 22:13:30 +0200

    Create component for last three recipes

---
+++ client/healthy-bite/src/app/shared/last-three-recipres/last-three-recipes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-last-three-recipes',
+  templateUrl: './last-three-recipes.component.html',
+  styleUrls: ['./last-three-recipes.component.css']
+})
+export class LastThreeRecipesComponent {
+
+}
--- client/healthy-bite/src/app/shared/shared.module.ts
+++ client/healthy-bite/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { SocialMediaIconsComponent } from './social-media-icons/social-media-ico
 import { ContactUsInfoComponent } from './contact-us-info/contact-us-info.component';
 import { HomeImageComponent } from './home-image/home-image.component';
 import { CatalogBtnComponent } from './catalog-btn/catalog-btn.component';
+import { LastThreeRecipesComponent } from './last-three-recipres/last-three-recipes.component';
 
 @NgModule({
-  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent],
+  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent, LastThreeRecipesComponent],
   imports: [CommonModule],
-  exports: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent],
+  exports: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent, LastThreeRecipesComponent],
 })
 export class SharedModule {}
