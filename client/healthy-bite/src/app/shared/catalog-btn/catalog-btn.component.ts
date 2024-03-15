3172112f13eece2d84c749caa95671e022dd4ad2
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 22:02:41 +0200

    Add catalog-btn component in shared module

---
+++ client/healthy-bite/src/app/shared/catalog-btn/catalog-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-btn',
+  templateUrl: './catalog-btn.component.html',
+  styleUrls: ['./catalog-btn.component.css']
+})
+export class CatalogBtnComponent {
+
+}
--- client/healthy-bite/src/app/shared/shared.module.ts
+++ client/healthy-bite/src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SocialMediaIconsComponent } from './social-media-icons/social-media-icons.component';
 import { ContactUsInfoComponent } from './contact-us-info/contact-us-info.component';
 import { HomeImageComponent } from './home-image/home-image.component';
+import { CatalogBtnComponent } from './catalog-btn/catalog-btn.component';
 
 @NgModule({
-  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent],
+  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent],
   imports: [CommonModule],
   exports: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent],
 })
