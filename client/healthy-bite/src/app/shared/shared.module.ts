3990e76fe29b4fd20191e4e9167126d85276a9dc
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 21:13:05 +0200

    Create shared module

---
+++ client/healthy-bite/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialMediaIconsComponent } from './social-media-icons/social-media-icons.component';
+
+
+
+@NgModule({
+  declarations: [
+    SocialMediaIconsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
