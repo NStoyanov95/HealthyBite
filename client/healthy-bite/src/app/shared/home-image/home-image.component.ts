5d6e513c6dc982478cc3e5dc917c1dab1bdb4eac
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 21:31:03 +0200

    Create home-image component and add image in assets

---
+++ client/healthy-bite/src/app/shared/home-image/home-image.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-image',
+  templateUrl: './home-image.component.html',
+  styleUrls: ['./home-image.component.css']
+})
+export class HomeImageComponent {
+
+}
