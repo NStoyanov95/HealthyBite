77797999f5e886e0af0578e217307ad9d2492d54
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 21:31:29 +0200

    Import shared module in app module and show home-image in homepage

--- client/healthy-bite/src/app/app.module.ts
+++ client/healthy-bite/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainModule } from './main/main.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MainModule } from './main/main.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainModule
+    MainModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/healthy-bite/src/app/shared/shared.module.ts
+++ client/healthy-bite/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialMediaIconsComponent } from './social-media-icons/social-media-icons.component';
 import { ContactUsInfoComponent } from './contact-us-info/contact-us-info.component';
+import { HomeImageComponent } from './home-image/home-image.component';
 
 @NgModule({
-  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent],
+  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent],
   imports: [CommonModule],
-  exports: [SocialMediaIconsComponent, ContactUsInfoComponent],
+  exports: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent],
 })
 export class SharedModule {}
