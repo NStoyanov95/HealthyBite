daf7ddd144dbb0b1a468138478324162fb410d0a
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-15 22:47:52 +0200

    Add register to router

--- client/healthy-bite/src/app/app-routing.module.ts
+++ client/healthy-bite/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
+  {path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
