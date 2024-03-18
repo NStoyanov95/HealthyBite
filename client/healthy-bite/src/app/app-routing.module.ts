8dc98817c2dcc82d0d31d29466af91adf6990a79
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-18 08:13:30 +0200

    Add path to login component

--- client/healthy-bite/src/app/app-routing.module.ts
+++ client/healthy-bite/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  {path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
