bdde5ee56aa701625e9fad7a4c1a3391df96d0ef
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-18 09:17:16 +0200

    Add HttpClient import to AuthService

--- client/healthy-bite/src/app/auth/auth.service.ts
+++ client/healthy-bite/src/app/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  
 }
