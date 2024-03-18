4af9466982d53adf96ac3871d2223a3604cf0381
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-18 12:09:32 +0200

    Generate catalog component

--- client/healthy-bite/src/app/recipe/recipe.module.ts
+++ client/healthy-bite/src/app/recipe/recipe.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CatalogComponent
+  ],
   imports: [CommonModule],
 })
 export class RecipeModule {}
