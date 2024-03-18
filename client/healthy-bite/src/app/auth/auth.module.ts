c8046b4f124875a8405a1c89de70ef1744c1ca68
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-18 08:09:53 +0200

    Generate login component

--- client/healthy-bite/src/app/auth/auth.module.ts
+++ client/healthy-bite/src/app/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
