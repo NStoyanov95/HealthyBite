ce7ebb86d8a969ceb418f19fd6b9bb445d6b43f1
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-18 08:40:22 +0200

    Add  reactive-driven approach to the login form

--- client/healthy-bite/src/app/auth/login/login.component.ts
+++ client/healthy-bite/src/app/auth/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private fb: FormBuilder) {}
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.minLength(5)]],
+  });
+
+  loginHandler() {
+    
+  }
 }
