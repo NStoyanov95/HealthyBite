682dcb6c6cbf9b1c26e5767503197b53d12c3e4e
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-19 10:08:11 +0200

    Fetch recipe details in ngOnInit

--- client/healthy-bite/src/app/recipe/recipe-details/recipe-details.component.ts
+++ client/healthy-bite/src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/types/recipe';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  styleUrls: ['./recipe-details.component.css'],
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+  recipe: Recipe | undefined;
 
+  constructor(
+    private recipeService: RecipeService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const recipeId = this.activatedRoute.snapshot.params['recipeId'];
+
+    this.recipeService.getSingleRecipe(recipeId).subscribe((data) => {
+      this.recipe = data;
+    });
+  }
 }
