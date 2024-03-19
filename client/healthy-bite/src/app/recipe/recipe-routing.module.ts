979e4909116d6342de2c9d825c79a93d264c7a4f
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-19 09:56:51 +0200

    Add recipe-details path

--- client/healthy-bite/src/app/recipe/recipe-routing.module.ts
+++ client/healthy-bite/src/app/recipe/recipe-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
       {
         path: 'catalog',
         component: CatalogComponent,
+      },
+      {
+        path: 'details/:recipeId',
+        component: RecipeDetailsComponent,
       },
     ],
   },
