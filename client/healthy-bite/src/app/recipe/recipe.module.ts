4a4d2c5d08eb349b53afecc231613bcc70878c72
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-19 09:54:09 +0200

    Create recipe details catalog

--- client/healthy-bite/src/app/recipe/recipe.module.ts
+++ client/healthy-bite/src/app/recipe/recipe.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CatalogComponent } from './catalog/catalog.component';
 import { RouterModule } from '@angular/router';
 import { RecipeRoutingModule } from './recipe-routing.module';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
-  declarations: [CatalogComponent],
+  declarations: [CatalogComponent, RecipeDetailsComponent],
   imports: [CommonModule,RecipeRoutingModule],
 })
 export class RecipeModule {}
