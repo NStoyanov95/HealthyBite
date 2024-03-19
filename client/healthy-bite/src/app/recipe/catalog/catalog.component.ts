cc45e1be92b0ca23b7ab16564bef9efc2304ead2
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-19 09:46:26 +0200

    Show catalog with dynamic data

--- client/healthy-bite/src/app/recipe/catalog/catalog.component.ts
+++ client/healthy-bite/src/app/recipe/catalog/catalog.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/types/recipe';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  recipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+
+    this.recipeService.getAllRecipes().subscribe((data)=>{
+      this.recipes = data
+    })
+  }
 }
--- client/healthy-bite/src/app/recipe/recipe.service.ts
+++ client/healthy-bite/src/app/recipe/recipe.service.ts
@@ -1,9 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { Recipe } from '../types/recipe';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipeService {
   constructor(private http: HttpClient) {}
+
+  getAllRecipes() {
+    return this.http.get<Recipe[]>(`${environment.apiUrl}/recipes/catalog`);
+  }
 }
