9867bf9ddcd0922af3ba26e3bb72d6e5e6d76dc5
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-19 10:27:05 +0200

    Show last three recipes with dynamic data

--- client/healthy-bite/src/app/shared/last-three-recipres/last-three-recipes.component.ts
+++ client/healthy-bite/src/app/shared/last-three-recipres/last-three-recipes.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/recipe/recipe.service';
+import { Recipe } from 'src/app/types/recipe';
 
 @Component({
   selector: 'app-last-three-recipes',
   templateUrl: './last-three-recipes.component.html',
-  styleUrls: ['./last-three-recipes.component.css']
+  styleUrls: ['./last-three-recipes.component.css'],
 })
-export class LastThreeRecipesComponent {
+export class LastThreeRecipesComponent implements OnInit {
+  recipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.recipeService.getLastThreeRecipes().subscribe((data) => {
+      this.recipes = data;
+    });
+  }
 }
--- client/healthy-bite/src/app/shared/shared.module.ts
+++ client/healthy-bite/src/app/shared/shared.module.ts
@@ -5,10 +5,23 @@ import { ContactUsInfoComponent } from './contact-us-info/contact-us-info.compon
 import { HomeImageComponent } from './home-image/home-image.component';
 import { CatalogBtnComponent } from './catalog-btn/catalog-btn.component';
 import { LastThreeRecipesComponent } from './last-three-recipres/last-three-recipes.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent, LastThreeRecipesComponent],
-  imports: [CommonModule],
-  exports: [SocialMediaIconsComponent, ContactUsInfoComponent, HomeImageComponent, CatalogBtnComponent, LastThreeRecipesComponent],
+  declarations: [
+    SocialMediaIconsComponent,
+    ContactUsInfoComponent,
+    HomeImageComponent,
+    CatalogBtnComponent,
+    LastThreeRecipesComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    SocialMediaIconsComponent,
+    ContactUsInfoComponent,
+    HomeImageComponent,
+    CatalogBtnComponent,
+    LastThreeRecipesComponent,
+  ],
 })
 export class SharedModule {}
