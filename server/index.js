5c7f7a848427102ae5d87031d907c28a4a7b32ea
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-20 08:40:35 +0200

    Add cors origin

--- server/index.js
+++ server/index.js
@@ -5,11 +5,17 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(routes);
 
-mongoose.connect("mongodb://localhost:27017/healthyBite")
-    .then(()=> console.log('db connected!'))
+mongoose
+  .connect("mongodb://localhost:27017/healthyBite")
+  .then(() => console.log("db connected!"));
 app.listen(3000, () => console.log("Server listening on port 3000..."));
