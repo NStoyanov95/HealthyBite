e2883c7a4f76297ab422a3ca51599cb2d943bb69
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-20 09:51:23 +0200

    Fix - send access token with response

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) => {
       userData
     );
     res.cookie("auth-cookie", accessToken, { httpOnly: true, secure: true });
-    res.status(200).send({ email, _id, username });
+    res.status(200).send({ email, _id, username,accessToken });
   } catch (error) {
     res.status(500).send({ error: "Internal Server Error" });
   }
@@ -23,7 +23,7 @@ router.post("/login", async (req, res) => {
       userData
     );
     res.cookie("auth-cookie", accessToken, { httpOnly: true, secure: true });
-    res.status(200).send({ email, _id, username });
+    res.status(200).send({ email, _id, username, accessToken });
   } catch (error) {
     res.status(401).send({ error: "Unauthorized" });
   }
