54caefc458c5ec80afebfe9c2ff844d7a61e2f1a
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-20 09:15:10 +0200

    Add cookie-parser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 const routes = require("./routes");
 
 const app = express();
+app.use(cookieParser())
 app.use(
   cors({
     origin: "http://localhost:4200",
