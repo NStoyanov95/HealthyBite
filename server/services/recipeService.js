eb96b4cc64086340a58d80d410c10eddaff808cd
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-20 10:46:14 +0200

    mplement deleteRecipe function in recipeService to delete recipe by id

--- server/services/recipeService.js
+++ server/services/recipeService.js
@@ -7,3 +7,5 @@ exports.getOne = (recipeId) => Recipe.findById(recipeId);
 exports.create = (recipeData) => Recipe.create(recipeData);
 
 exports.getLastThree = () => Recipe.find().sort({ createdAt: -1 }).limit(3);
+
+exports.deleteRecipe = () => Recipe.findByIdAndDelete();
