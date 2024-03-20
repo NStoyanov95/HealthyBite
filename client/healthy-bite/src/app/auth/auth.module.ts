8d7e5c03d5fe36c03a266b7411911c0c54e43383
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-20 21:06:34 +0200

    Create profile component

--- client/healthy-bite/src/app/auth/auth.module.ts
+++ client/healthy-bite/src/app/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
