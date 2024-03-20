39739aafc7c0cca0253ddb1b46316007dbbbd869
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-20 09:40:44 +0200

    Handle logout request and clear cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,9 @@ const userService = require("../services/userService");
 router.post("/register", async (req, res) => {
   const userData = req.body;
   try {
-    const { email, username, _id, accessToken } = await userService.register(userData);
+    const { email, username, _id, accessToken } = await userService.register(
+      userData
+    );
     res.cookie("auth-cookie", accessToken, { httpOnly: true, secure: true });
     res.status(200).send({ email, _id, username });
   } catch (error) {
@@ -24,6 +26,15 @@ router.post("/login", async (req, res) => {
     res.status(200).send({ email, _id, username });
   } catch (error) {
     res.status(401).send({ error: "Unauthorized" });
+  }
+});
+
+router.post("/logout", (req, res) => {
+  try {
+    res.clearCookie("auth-cookie");
+    res.status(200).send({ message: "Logout successfully" });
+  } catch (error) {
+    res.status(500).send({ error: "Logout Failed" });
   }
 });
 
