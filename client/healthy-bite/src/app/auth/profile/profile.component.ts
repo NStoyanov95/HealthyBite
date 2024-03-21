1d6d11b130d6606303a4fdc7ae81495f335667ab
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-21 09:41:31 +0200

    Implemented user profile retrieval in ngOnInit

--- client/healthy-bite/src/app/auth/profile/profile.component.ts
+++ client/healthy-bite/src/app/auth/profile/profile.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserProfile } from 'src/app/types/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userId: string = '';
+  userProfile!: UserProfile;
 
+  constructor(
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = this.activatedRoute.snapshot.params['userId'];
+    this.authService.getSingleUser(this.userId).subscribe({
+      next: (data) => {
+          this.userProfile = data;
+          console.log(this.userProfile);
+      },
+      error: (error) => {
+        console.log('HTTP error', error);
+        alert('HTTP error ' + error);
+      },
+    });
+  }
 }
