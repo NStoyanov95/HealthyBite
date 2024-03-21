333d4c917adb291cac78e5f5435c268b5e9fa31c
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-21 09:40:54 +0200

    Defined interfaces for user profile and recipe data

--- client/healthy-bite/src/app/types/user.ts
+++ client/healthy-bite/src/app/types/user.ts
@@ -4,8 +4,28 @@ export interface User {
   username: string;
   accessToken?: string;
   favorite: string[];
+  error?: string;
 }
 
 export interface FavoriteResponse {
-  _id: string
+  _id: string;
+  error?: string;
+}
+
+export interface UserProfile {
+  _id: string;
+  username: string;
+  email: string;
+  favorite: Recipe[];
+  error?: string;
+}
+
+interface Recipe {
+  _id: string;
+  recipeName: string;
+  imageUrl: string;
+  ingredients: string[];
+  instructions: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
