fd425935e41eb640ddb88ceb08cd69d00024d04b
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-21 09:51:16 +0200

    Implemented OnDestroy lifecycle hook

--- client/healthy-bite/src/app/auth/profile/profile.component.ts
+++ client/healthy-bite/src/app/auth/profile/profile.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { UserProfile } from 'src/app/types/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
   userId: string = '';
   userProfile!: UserProfile;
+  subscription!: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -21,13 +23,17 @@ export class ProfileComponent implements OnInit {
     this.userId = this.activatedRoute.snapshot.params['userId'];
     this.authService.getSingleUser(this.userId).subscribe({
       next: (data) => {
-          this.userProfile = data;
-          console.log(this.userProfile);
+        this.userProfile = data;
+        console.log(this.userProfile);
       },
       error: (error) => {
         console.log('HTTP error', error);
         alert('HTTP error ' + error);
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
