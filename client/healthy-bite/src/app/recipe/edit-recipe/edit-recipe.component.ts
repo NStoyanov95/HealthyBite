22821a2baed1bad92126745c3a09cd0e65cd6026
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-23 21:25:24 +0200

    Initialized form, added validators, and enabled ingredient addition.

--- client/healthy-bite/src/app/recipe/edit-recipe/edit-recipe.component.ts
+++ client/healthy-bite/src/app/recipe/edit-recipe/edit-recipe.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-recipe',
   templateUrl: './edit-recipe.component.html',
-  styleUrls: ['./edit-recipe.component.css']
+  styleUrls: ['./edit-recipe.component.css'],
 })
 export class EditRecipeComponent {
+  constructor(private fb: FormBuilder) {}
 
+  editRecipeForm = this.fb.group({
+    recipeName: ['', Validators.required],
+    imageUrl: ['', Validators.required],
+    ingredients: this.fb.array([], Validators.required),
+    instructions: ['', Validators.required],
+  });
+
+  get ingredientControls() {
+    return (this.editRecipeForm.get('ingredients') as FormArray).controls;
+  }
+
+  addIngredients() {
+    const ingredients = this.editRecipeForm.get('ingredients') as FormArray;
+    if (ingredients.length >= 5) {
+      return;
+    }
+    ingredients.push(this.fb.control('', Validators.required));
+  }
 }
