2d1daeb2341e870014e6913b2e113e68740a1b50
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-23 20:41:41 +0200

    Create edit recipe component

---
+++ client/healthy-bite/src/app/recipe/edit-recipe/edit-recipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-recipe',
+  templateUrl: './edit-recipe.component.html',
+  styleUrls: ['./edit-recipe.component.css']
+})
+export class EditRecipeComponent {
+
+}
--- client/healthy-bite/src/app/recipe/recipe.module.ts
+++ client/healthy-bite/src/app/recipe/recipe.module.ts
@@ -5,9 +5,10 @@ import { RecipeRoutingModule } from './recipe-routing.module';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EditRecipeComponent } from './edit-recipe/edit-recipe.component';
 
 @NgModule({
-  declarations: [CatalogComponent, RecipeDetailsComponent, CreateRecipeComponent],
+  declarations: [CatalogComponent, RecipeDetailsComponent, CreateRecipeComponent, EditRecipeComponent],
   imports: [CommonModule,RecipeRoutingModule, ReactiveFormsModule],
 })
 export class RecipeModule {}
