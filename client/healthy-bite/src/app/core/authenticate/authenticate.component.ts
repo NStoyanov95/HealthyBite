ba739fe1a2c2add01da2dd87e15a4f1232b072b0
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-25 00:01:56 +0200

    Set isAuthenticating to false after profile retrieval in AuthenticateComponent

--- client/healthy-bite/src/app/core/authenticate/authenticate.component.ts
+++ client/healthy-bite/src/app/core/authenticate/authenticate.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/auth/user.service';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
-  styleUrls: ['./authenticate.component.css']
+  styleUrls: ['./authenticate.component.css'],
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+  isAuthenticating: boolean = true;
+  
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe({
+      next: () => {
+        this.isAuthenticating = false;
+      },
+      error: () => {
+        this.isAuthenticating = false;
+      },
+      complete: () => {
+        this.isAuthenticating = false;
+      },
+    });
+  }
 }
