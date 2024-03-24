c85994b5519132f7d0c5995c9646eef4e594ffda
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-24 20:49:40 +0200

    Implement lazy loading for the users route by dynamically importing the AuthModule.

--- client/healthy-bite/src/app/app-routing.module.ts
+++ client/healthy-bite/src/app/app-routing.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { AuthActivate } from './core/guards/auth.guard';
-import { ProfileComponent } from './auth/profile/profile.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   {
-    path: 'register',
-    component: RegisterComponent,
-    canActivate: [AuthActivate],
-  },
-  { path: 'login', component: LoginComponent, canActivate: [AuthActivate] },
-  {
-    path: 'profile/:userId',
-    component: ProfileComponent,
-    canActivate: [AuthActivate],
+    path: 'users',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
 ];
 
--- client/healthy-bite/src/app/auth/auth.module.ts
+++ client/healthy-bite/src/app/auth/auth.module.ts
@@ -4,19 +4,12 @@ import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
-
-
+import { RouterModule } from '@angular/router';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [
-    RegisterComponent,
-    LoginComponent,
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
-  exports: [RegisterComponent]
+  declarations: [RegisterComponent, LoginComponent, ProfileComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, AuthRoutingModule],
+  exports: [RegisterComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
