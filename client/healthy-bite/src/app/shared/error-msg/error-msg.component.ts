a93c4ad9ec6139c42122ceb9bb6ccf3f56ed28a3
NStoyanov95 <nstoyanov9512@gmail.com>
2024-03-25 17:18:35 +0200

    Add markup and styles to error-message component

--- client/healthy-bite/src/app/shared/error-msg/error-msg.component.ts
+++ client/healthy-bite/src/app/shared/error-msg/error-msg.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-error-msg',
   templateUrl: './error-msg.component.html',
-  styleUrls: ['./error-msg.component.css']
+  styleUrls: ['./error-msg.component.css'],
 })
 export class ErrorMsgComponent {
-
+  @Input() errorMessage: string | undefined = undefined;
 }
