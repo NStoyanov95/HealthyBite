ef7adc2619f028f90a5b3afac12012f1fe277c0d
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-23 19:06:04 +0300

    Generate blog module

---
+++ client/healthy-bite/src/app/blog/blog.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemesComponent } from './themes/themes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ThemesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
