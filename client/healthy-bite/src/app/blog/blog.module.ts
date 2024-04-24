ff5db598e3f583aa94f638da538ac4c46368c6e4
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-24 21:09:57 +0300

    Generate blog theme card component

--- client/healthy-bite/src/app/blog/blog.module.ts
+++ client/healthy-bite/src/app/blog/blog.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemesComponent } from './themes/themes.component';
 import { BlogRoutingModule } from './blog-routing.module';
+import { ThemeCardComponent } from './theme-card/theme-card.component';
 
 @NgModule({
-  declarations: [ThemesComponent],
+  declarations: [ThemesComponent, ThemeCardComponent],
   imports: [CommonModule, BlogRoutingModule],
 })
 export class BlogModule {}
---
+++ client/healthy-bite/src/app/blog/theme-card/theme-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-card',
+  templateUrl: './theme-card.component.html',
+  styleUrls: ['./theme-card.component.css']
+})
+export class ThemeCardComponent {
+
+}
