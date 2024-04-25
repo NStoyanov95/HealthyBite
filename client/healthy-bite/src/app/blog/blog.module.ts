b00c790dd8c32dad0f188b2c1babfee8675ee8b7
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-25 18:25:51 +0300

    Create mini navbar in blog section

--- client/healthy-bite/src/app/blog/blog.module.ts
+++ client/healthy-bite/src/app/blog/blog.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ThemesComponent } from './themes/themes.component';
 import { BlogRoutingModule } from './blog-routing.module';
 import { ThemeCardComponent } from './theme-card/theme-card.component';
+import { MiniNavbarComponent } from './mini-navbar/mini-navbar.component';
 
 @NgModule({
-  declarations: [ThemesComponent, ThemeCardComponent],
+  declarations: [ThemesComponent, ThemeCardComponent, MiniNavbarComponent],
   imports: [CommonModule, BlogRoutingModule],
 })
 export class BlogModule {}
---
+++ client/healthy-bite/src/app/blog/mini-navbar/mini-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-navbar',
+  templateUrl: './mini-navbar.component.html',
+  styleUrls: ['./mini-navbar.component.css']
+})
+export class MiniNavbarComponent {
+
+}
