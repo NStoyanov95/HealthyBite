075aaf8cf4eb633f3250a9c374a5e8524fec147f
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-25 19:02:05 +0300

    Add reactive form logic to form constructor

--- client/healthy-bite/src/app/blog/blog.module.ts
+++ client/healthy-bite/src/app/blog/blog.module.ts
@@ -5,9 +5,15 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { ThemeCardComponent } from './theme-card/theme-card.component';
 import { MiniNavbarComponent } from './mini-navbar/mini-navbar.component';
 import { CreateThemeComponent } from './create-theme/create-theme.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ThemesComponent, ThemeCardComponent, MiniNavbarComponent, CreateThemeComponent],
-  imports: [CommonModule, BlogRoutingModule],
+  declarations: [
+    ThemesComponent,
+    ThemeCardComponent,
+    MiniNavbarComponent,
+    CreateThemeComponent,
+  ],
+  imports: [CommonModule, BlogRoutingModule, ReactiveFormsModule],
 })
 export class BlogModule {}
--- client/healthy-bite/src/app/blog/create-theme/create-theme.component.ts
+++ client/healthy-bite/src/app/blog/create-theme/create-theme.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-theme',
   templateUrl: './create-theme.component.html',
-  styleUrls: ['./create-theme.component.css']
+  styleUrls: ['./create-theme.component.css'],
 })
 export class CreateThemeComponent {
+  constructor(private fb: FormBuilder) {}
 
+  createThemeForm = this.fb.group({
+    themeTitle: ['', Validators.required],
+    themeImage: ['', Validators.required],
+    shortDescription: ['', Validators.required],
+    description: ['', Validators.required],
+  });
+
+  createThemeHandler() {
+    //
+  }
 }
