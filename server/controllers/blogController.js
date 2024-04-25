a5804cef9b01ac94802460b264eaa78f5ec57e63
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-25 20:09:15 +0300

    Implement fetch all themes logic

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 
 const blogService = require("../services/blogService");
 
+router.get("/themes", async (req, res) => {
+  try {
+    const themes = await blogService.getAll();
+    return res.json(themes);
+  } catch (error) {
+    console.log("Error fetch themes:", error);
+    res.status(500).json({ error });
+  }
+});
+
 router.post("/create", async (req, res) => {
   try {
     const themeData = req.body.themeData;
--- server/services/blogService.js
+++ server/services/blogService.js
@@ -1,3 +1,5 @@
 const Blog = require("../models/Blog");
 
 exports.create = (blogData) => Blog.create(blogData);
+
+exports.getAll = () => Blog.find();
