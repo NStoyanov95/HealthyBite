70db16dd13ea067b248b99f294cb08aa5d6805d4
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-25 18:30:28 +0300

    Generate create blog theme component

--- client/healthy-bite/src/app/blog/blog.module.ts
+++ client/healthy-bite/src/app/blog/blog.module.ts
@@ -4,9 +4,10 @@ import { ThemesComponent } from './themes/themes.component';
 import { BlogRoutingModule } from './blog-routing.module';
 import { ThemeCardComponent } from './theme-card/theme-card.component';
 import { MiniNavbarComponent } from './mini-navbar/mini-navbar.component';
+import { CreateThemeComponent } from './create-theme/create-theme.component';
 
 @NgModule({
-  declarations: [ThemesComponent, ThemeCardComponent, MiniNavbarComponent],
+  declarations: [ThemesComponent, ThemeCardComponent, MiniNavbarComponent, CreateThemeComponent],
   imports: [CommonModule, BlogRoutingModule],
 })
 export class BlogModule {}
---
+++ client/healthy-bite/src/app/blog/create-theme/create-theme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-theme',
+  templateUrl: './create-theme.component.html',
+  styleUrls: ['./create-theme.component.css']
+})
+export class CreateThemeComponent {
+
+}
