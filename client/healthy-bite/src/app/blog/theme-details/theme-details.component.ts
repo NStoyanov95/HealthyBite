02b113cd9d5235d86722f8479893f8f000179ed0
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-27 21:07:03 +0300

    Generate theme details component

---
+++ client/healthy-bite/src/app/blog/theme-details/theme-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-details',
+  templateUrl: './theme-details.component.html',
+  styleUrls: ['./theme-details.component.css']
+})
+export class ThemeDetailsComponent {
+
+}
