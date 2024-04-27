4fb589b8f84fd1fca071b2fb6b935443a00589bd
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-27 21:07:26 +0300

    Generate theme details component

--- client/healthy-bite/src/app/blog/blog.module.ts
+++ client/healthy-bite/src/app/blog/blog.module.ts
@@ -6,6 +6,7 @@ import { ThemeCardComponent } from './theme-card/theme-card.component';
 import { MiniNavbarComponent } from './mini-navbar/mini-navbar.component';
 import { CreateThemeComponent } from './create-theme/create-theme.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ThemeDetailsComponent } from './theme-details/theme-details.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ThemeCardComponent,
     MiniNavbarComponent,
     CreateThemeComponent,
+    ThemeDetailsComponent,
   ],
   imports: [CommonModule, BlogRoutingModule, ReactiveFormsModule],
 })
